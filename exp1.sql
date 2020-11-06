a26b667664366cc4c5a7244360856d859321098d
Manu Mathew <60087043+ceccs18c37@users.noreply.github.com>
2020-11-06 15:43:55 +0530

    Update exp1.sql

--- exp1.sql
+++ exp1.sql
@@ -1,5 +1,6 @@
 USE lab;
 
+#a
 CREATE TABLE student (
 	sno INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -7,14 +8,20 @@ CREATE TABLE student (
     dept VARCHAR(20) NOT NULL
 );
 
+#b
 ALTER TABLE student ADD COLUMN age INT NOT NULL;
 
+#c
 ALTER TABLE student MODIFY COLUMN dept VARCHAR(10); 
 
+#d
 ALTER TABLE student DROP COLUMN marks;
 
+#e
 RENAME TABLE student TO students;
 
+#f
 TRUNCATE TABLE students;
 
+#g
 DROP TABLE students;
